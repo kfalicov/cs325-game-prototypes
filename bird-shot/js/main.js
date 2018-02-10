cbd26e66f17783cce01eded98d34a391f3b91564
kfalicov <kfalicov@gmail.com>
2018-02-10 18:40:16 -0500

    More robust name generator

--- bird-shot/js/main.js
+++ bird-shot/js/main.js
@@ -13,6 +13,8 @@ window.onload = function() {
 	// the high score if you want.
 	var shared = {};
 	
+	generateName();
+
 	game.state.add( 'Boot', GameStates.makeBoot( game ) );
 	game.state.add( 'Preloader', GameStates.makePreloader( game ) );
 	game.state.add( 'MainMenu', GameStates.makeMainMenu( game, shared ) );
@@ -22,11 +24,12 @@ window.onload = function() {
 	game.state.start('Boot');
 };
 
-var adjective = ["Dirty","Mild","Wild","Crazy","Fast","Quick","Red","Bad","Mad","Old","Waco","Saddle","Boots","Spurs","Buffalo","Mexico","Scary","Horse","Axe","Western","Arizona","Colorado","Rocky"];
-var title = ["Doc","Miner","Sheriff","Banker","Rancher","Cowboy","Kid","Farmer","Murderer","Bandit","Outlaw","Engineer","Inventor","Chicken","President","Mayor","Bartender","Soldier","Indian","Tanner","Missus","Doctor","Mistress","Master","Poor Man"];
-var firstname = ["Charlie","Rich","LeBron","Harry","Clint","McGraw","Sam","Wylde","Wyatt","Buford","Woody","Buchanan","Gene","Bart","Billy","Butch","John","George","Nat","Clyde"];
-var lastname = ["Harding","Love","King","Earp","James","Eastwood","McGraw","Hill","Brown","Weston","Barnes","Goodman","Woodson","Harrison","Johnson","Oldman","Washington"];
-var format = ["$f \"$a\"|$n $l", "$f $l|$n", "$f|$n The $a|$n $t", "$t $f|$l", "$f \"The $t\" $l"];
+var adjective = ["Dirty","Mild","Wild","Crazy","Fast","Quick","Red","Bad","Mad","Old","Waco","Scary","Western","Gold","Rocky","Greasy","Mean","Drunk"];
+var nickname = ["Railway","Locomotive","Potato","Wagon","Steel","Saddle","Boots","Buffalo","Mexico","Horse","Axe","Arizona","Colorado","Spurs","Whiskey","Chicken"];
+var title = ["Doc","Miner","Sheriff","Banker","Rancher","Cowboy","Kid","Farmer","Murderer","Bandit","Outlaw","Chicken","Mayor","Bartender","Blacksmith","Indian","Drunkard","Doctor","Beggar"];
+var firstname = ["Charlie","Warren","Owen","Rich","LeBron","Harry","Clint","McGraw","Sam","Wylde","Wyatt","Buford","Woody","Buchanan","Gene","Bart","Billy","Butch","John","George","Nat","Clyde"];
+var lastname = ["Wilder","Harding","Love","Wilson","King","Earp","James","Eastwood","McGraw","Hill","Brown","Weston","Barnes","Goodman","Woodson","Harrison","Johnson","Oldman","Washington"];
+var format = ["$a ($f|$l)", "$f( $l|$n)", "$f $l", "$f the ($a |$n)$t", "\"$g\" ($f|$l)", "$f \"(The $t|$a)\" $l", "$f \"($a |$n)$g\" $l"];
 
 /*
 * global functions used for random name generation
@@ -34,32 +37,43 @@ var format = ["$f \"$a\"|$n $l", "$f $l|$n", "$f|$n The $a|$n $t", "$t $f|$l", "
 function generateName() {
 	let string = format[Math.floor(Math.random() * format.length)];
 	let result = removeOr(string);
-	result = removeOr(result);
-	result = removeOr(result);
+	let count = 0;
+	while(result.includes("|") && count<5){
+		result=removeOr(result);
+		count++;
+	}
+	//console.log("count = "+count);
 	result = stringFill(result);
 	return result;
 }
 
+//goes through the string and removes each set of parentheses at a time. designed to be run multiple times to handle multiple or statements in a string.
 function removeOr(string){
 	let result;
-	if(Math.random() < 0.5){
-		result = string.replace(/(\"\$.\"|\$.)\|/, '');
+	if(Math.random() < 0.5 || true){
+		result = string.replace(/\(.{0,6}\|/, '');
+		result = result.replace(/\)/, '');
 	}
 	else{
-		result = string.replace(/\|\$./, '');
+		result = string.replace(/\|.{0,6}\)/, '');
+		result = result.replace(/\(/, '');
 	}
+	//console.log(result);
 	return result;
 }
 
+//replaces set strings with data
 function stringFill(string){
-	let result = string.replace("$a", randElement(adjective));
-	result = result.replace(/\$n ?/g,'');
+	let result = string.replace(/\$n/g,'');
+	result = result.replace("$a", randElement(adjective));
 	result = result.replace("$t", randElement(title));
 	result = result.replace("$f", randElement(firstname));
 	result = result.replace("$l", randElement(lastname));
+	result = result.replace("$g", randElement(nickname));
 	return result;
 }
 
+//a helper function to get items from lists
 function randElement(item){
 	return item[Math.floor(Math.random() * item.length)];
 }
